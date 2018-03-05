55f949a3d2c28b67fdbe7e68c27ef9eecdfdd810
Jacob Bolda <jbolda@gmail.com>
2018-03-04 22:38:14 -0600

    remove accidental submodules

---
+++ plugins/gatsby-theme-bulma-layout/Simple/components/SimpleTopNav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "gatsby-link";
+import styles from "../palette.json";
+
+class SimpleTopNav extends React.Component {
+  render() {
+    return (
+      <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link
+            to="/"
+            className="navbar-item"
+            style={this.props.location.pathname == '/' ? {backgroundColor: styles.colors.P5} : {}}
+            >
+            <span
+              className="title"
+              style={this.props.location.pathname == '/' ? {color: styles.colors.P2} : {color: styles.colors.P5}}
+              >
+              AECC
+            </span>
+          </Link>
+          <Link
+            to="/code-of-conduct/"
+            className="navbar-item"
+            style={this.props.location.pathname == '/code-of-conduct/' ? {backgroundColor: styles.colors.P5, color: styles.colors.P2} : {}}
+            >
+            Code of Conduct
+          </Link>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default SimpleTopNav;
