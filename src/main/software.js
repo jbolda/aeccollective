6f07f0ab6089194eadc7c133dcba96787fb489a7
Jacob Bolda <me@jacobbolda.com>
2018-08-17 23:45:51 -0500

    add in (pseudocode for) tags on /software/

--- src/main/software.js
+++ src/main/software.js
@@ -108,6 +108,7 @@ const softwareTable = data => (
           </p>
           <div>
             <p>{edge.node.frontmatter.description}</p>
+            <div>{edge.node.tags.map(tag => <span className="tag is-primary">{tag}</span>)}</div>
             <Link to={edge.node.frontmatter.path}>
               <button className="button is-info">Learn More</button>
             </Link>
@@ -185,6 +186,7 @@ export const pageQuery = graphql`
             path
             title
             discipline
+            tags
             professionalPricing
             description
             website
