b3686c98ec9b839d8390b266cac2c8d26dd4b591
Jacob Bolda <me@jacobbolda.com>
2021-02-20 20:47:20 -0600

    switch to command text choices, split commands

--- commands/init-command.js
+++ commands/init-command.js
@@ -9,35 +9,182 @@ const run = async () => {
 
   const url = `${discord_api}/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`;
 
-  const bodyContent = {
-    name: 'role',
-    description:
-      'Ask the the bot to assign me a role. You may run this again to add another.',
-    options: [
-      {
-        name: 'role-to-set-me-as',
-        description: 'Choose your role. You may run this again to add another.',
-        type: 8, // 8 is type ROLE
-        required: true
-      }
-    ]
-  };
+  const contents = [
+    {
+      name: 'role',
+      description:
+        'Ask the the bot to assign me a role. You may run this again to add another.',
+      options: [
+        {
+          name: 'role-to-set-me-as',
+          description:
+            'Choose your role. You may run this again to add another.',
+          type: 3, // 3 is string
+          required: true,
+          choices: [
+            {
+              name: 'Student',
+              value: 'Student'
+            },
+            {
+              name: 'Professional',
+              value: 'Professional'
+            },
+            {
+              name: 'Business Administration',
+              value: 'Business Administration'
+            },
+            {
+              name: 'BIM Specialist',
+              value: 'BIM Specialist'
+            },
+            {
+              name: 'Interested',
+              value: 'Interested'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'field',
+      description:
+        'Ask the bot to assign a field the represents your background. You may run this again to add another.',
+      options: [
+        {
+          name: 'field-to-set-me-as',
+          description:
+            'Choose your role. You may run this again to add another.',
+          type: 3, // 3 is string
+          required: true,
+          choices: [
+            {
+              name: 'Architecture',
+              value: 'Architecture'
+            },
+            {
+              name: 'Engineering',
+              value: 'Engineering'
+            },
+            {
+              name: 'Structural',
+              value: 'Structural'
+            },
+            {
+              name: 'Electrical',
+              value: 'Electrical'
+            },
+            {
+              name: 'Mechanical',
+              value: 'Mechanical'
+            },
+            {
+              name: 'Construction',
+              value: 'Construction'
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'remove',
+      description: 'Remove a role or a field.',
+      options: [
+        {
+          name: 'role',
+          description: 'Ask the bot to remove a previously requested role.',
+          type: 1, // 1 is type SUB_COMMAND
+          options: [
+            {
+              name: 'role-to-set-me-as',
+              description: 'Remove a role that you previously requested.',
+              type: 3, // 3 is string
+              required: true,
+              choices: [
+                {
+                  name: 'Student',
+                  value: 'Student'
+                },
+                {
+                  name: 'Professional',
+                  value: 'Professional'
+                },
+                {
+                  name: 'Business Administration',
+                  value: 'Business Administration'
+                },
+                {
+                  name: 'BIM Specialist',
+                  value: 'BIM Specialist'
+                },
+                {
+                  name: 'Interested',
+                  value: 'Interested'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'field',
+          description: 'Ask the bot to remove a previously requested field.',
+          type: 1, // 1 is type SUB_COMMAND
+          options: [
+            {
+              name: 'role-to-set-me-as',
+              description: 'Remove a field that you previously assigned.',
+              type: 3, // 3 is string
+              required: true,
+              choices: [
+                {
+                  name: 'Architecture',
+                  value: 'Architecture'
+                },
+                {
+                  name: 'Engineering',
+                  value: 'Engineering'
+                },
+                {
+                  name: 'Structural',
+                  value: 'Structural'
+                },
+                {
+                  name: 'Electrical',
+                  value: 'Electrical'
+                },
+                {
+                  name: 'Mechanical',
+                  value: 'Mechanical'
+                },
+                {
+                  name: 'Construction',
+                  value: 'Construction'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
 
   const headers = {
     'Content-Type': 'application/json',
     Authorization: `Bot ${DISCORD_BOT_TOKEN}`
   };
 
-  const json = JSON.stringify(bodyContent);
+  for (let bodyContent of contents) {
+    const json = JSON.stringify(bodyContent);
 
-  const response = await fetch(url, {
-    method: 'post',
-    body: json,
-    headers
-  });
-  const data = await response.json();
+    const response = await fetch(url, {
+      method: 'post',
+      body: json,
+      headers
+    });
+    const data = await response.json();
 
-  console.dir(data);
+    console.dir(data);
+  }
 };
 
 run();
