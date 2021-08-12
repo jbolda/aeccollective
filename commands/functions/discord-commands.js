e62fd0f46651b4d269bf59e06c344e23e76c2328
Jacob Bolda <me@jacobbolda.com>
2021-08-12 03:25:58 -0500

    content type default changed, explicitly set json

--- commands/functions/discord-commands.js
+++ commands/functions/discord-commands.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch');
-const interactions = require('discord-interactions');
+const { verifyKey } = require('discord-interactions');
 
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
 
@@ -12,37 +12,44 @@ const headers = {
 };
 
 exports.handler = async function (event, context) {
-  const body = JSON.parse(event.body);
-  try {
-    const signature = event.headers['x-signature-ed25519'];
-    const timestamp = event.headers['x-signature-timestamp'];
-    const isValidRequest = interactions.verifyKey(
-      event.body,
-      signature,
-      timestamp,
-      BOT_PUBLIC_KEY
-    );
-    if (!isValidRequest) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ message: 'Bad request signature' })
-      };
-    }
-  } catch (e) {
+  if (!DISCORD_BOT_TOKEN) {
+    console.error(`DISCORD_BOT_TOKEN is not defined.`);
+  }
+  if (!BOT_PUBLIC_KEY) {
+    console.error(`BOT_PUBLIC_KEY is not defined.`);
+  }
+
+  if (event.httpMethod !== 'POST') {
     return {
       statusCode: 401,
-      body: JSON.stringify({ message: 'Bad request signature' })
+      body: JSON.stringify({
+        type: 2,
+        content: 'bad post'
+      })
     };
+  }
+
+  const body = JSON.parse(event.body);
+  const signature = event.headers['x-signature-ed25519'];
+  const timestamp = event.headers['x-signature-timestamp'];
+  const isValidRequest = verifyKey(
+    event.body,
+    signature,
+    timestamp,
+    BOT_PUBLIC_KEY
+  );
+
+  if (!isValidRequest) {
+    return jsonify({
+      code: 401,
+      type: 2,
+      data: { content: 'Bad request signature' }
+    });
   }
 
   // return the ping (discord checking if service is active)
   if (body.type === 1) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        type: 1
-      })
-    };
+    return jsonify({ code: 200, type: 1, data: {} });
   }
 
   // for reference
@@ -71,7 +78,10 @@ exports.handler = async function (event, context) {
     ['Construction', '420954975083036682']
   ]);
 
-  if (body.data.name === 'role' || body.data.name === 'field') {
+  if (
+    body.type === 2 &&
+    (body.data.name === 'role' || body.data.name === 'field')
+  ) {
     const requestedRole = body.data.options[0].value;
     const userID = body.member.user.id;
 
@@ -86,45 +96,39 @@ exports.handler = async function (event, context) {
         });
       } catch (e) {
         console.error(e);
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            type: 4,
-            data: {
-              tts: false,
-              content: `Error setting permissions.`,
-              embeds: [],
-              allowed_mentions: []
-            }
-          })
-        };
+        return jsonify({
+          code: 200,
+          type: 4,
+          data: {
+            tts: false,
+            content: `Error setting permissions.`,
+            embeds: [],
+            allowed_mentions: []
+          }
+        });
       }
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          type: 4,
-          data: {
-            tts: false,
-            content: `${requestedRole} has been added to <@${body.member.user.id}>.`,
-            embeds: [],
-            allowed_mentions: { parse: ['users'] }
-          }
-        })
-      };
+      return jsonify({
+        code: 200,
+        type: 4,
+        data: {
+          tts: false,
+          content: `${requestedRole} has been added to <@${body.member.user.id}>.`,
+          embeds: [],
+          allowed_mentions: { parse: ['users'] }
+        }
+      });
     } else {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          type: 4,
-          data: {
-            tts: false,
-            content: `Sorry, <@${body.member.user.id}>, ${requestedRole} is not an allowed option to self assign.`,
-            embeds: [],
-            allowed_mentions: { parse: ['users'] }
-          }
-        })
-      };
+      return jsonify({
+        code: 200,
+        type: 4,
+        data: {
+          tts: false,
+          content: `Sorry, <@${body.member.user.id}>, ${requestedRole} is not an allowed option to self assign.`,
+          embeds: [],
+          allowed_mentions: { parse: ['users'] }
+        }
+      });
     }
   } else if (body.data.name === 'remove') {
     const roleToRemove = body.data.options[0].options[0].value;
@@ -141,58 +145,64 @@ exports.handler = async function (event, context) {
         });
       } catch (e) {
         console.error(e);
-        return {
-          statusCode: 200,
-          body: JSON.stringify({
-            type: 4,
-            data: {
-              tts: false,
-              content: `Error removing permissions.`,
-              embeds: [],
-              allowed_mentions: []
-            }
-          })
-        };
+        return jsonify({
+          code: 200,
+          type: 4,
+          data: {
+            tts: false,
+            content: `Error removing permissions.`,
+            embeds: [],
+            allowed_mentions: []
+          }
+        });
       }
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          type: 4,
-          data: {
-            tts: false,
-            content: `${roleToRemove} has been removed from <@${body.member.user.id}>.`,
-            embeds: [],
-            allowed_mentions: { parse: ['users'] }
-          }
-        })
-      };
+      return jsonify({
+        code: 200,
+        type: 4,
+        data: {
+          tts: false,
+          content: `${roleToRemove} has been removed from <@${body.member.user.id}>.`,
+          embeds: [],
+          allowed_mentions: { parse: ['users'] }
+        }
+      });
     } else {
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          type: 4,
-          data: {
-            tts: false,
-            content: `Sorry, <@${body.member.user.id}>, ${roleToRemove} cannot be removed.`,
-            embeds: [],
-            allowed_mentions: { parse: ['users'] }
-          }
-        })
-      };
+      return jsonify({
+        code: 200,
+        type: 4,
+        data: {
+          tts: false,
+          content: `Sorry, <@${body.member.user.id}>, ${roleToRemove} cannot be removed.`,
+          embeds: [],
+          allowed_mentions: { parse: ['users'] }
+        }
+      });
     }
   }
 
+  return jsonify({
+    code: 200,
+    type: 4,
+    data: {
+      tts: false,
+      content: 'I am not certain what to do with this...',
+      embeds: [],
+      allowed_mentions: []
+    }
+  });
+};
+
+const jsonify = ({ type, code, data }) => {
+  console.log({ type, code, data });
   return {
-    statusCode: 200,
+    statusCode: code,
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({
-      type: 4,
-      data: {
-        tts: false,
-        content: 'I am not certain what to do with this...',
-        embeds: [],
-        allowed_mentions: []
-      }
+      type: type,
+      data
     })
   };
 };
